068085e2410bcd10be006afbe7f6019b168c3461
DEV-ZTX <sucidaldazai777@gmail.com>
2025-04-03 00:12:44 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -78,13 +78,12 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
     offset = int(update.inline_query.offset) if update.inline_query.offset else 0
 
     if query.startswith('collection.'):
-        user_id, *search_terms = query.split(' ')[0].split('.')[1], ' '.join(query.split(' ')[1:])
+        user_id = str(query.split(' ')[0].split('.')[1])  # Ensure it's a string, ' '.join(query.split(' ')[1:])
         if user_id.isdigit():
             if user_id in user_collection_cache:
                 user = user_collection_cache[user_id]
             else:
-                user = await user_collection.find_one({'id': int(user_id)})
-                user_collection_cache[user_id] = user
+                user = await user_collection.find_one({'id': int(user_id)}) if user_id.isdigit() else None
 
             if user:
                 all_characters = list({v['id']:v for v in user['characters']}.values())
@@ -121,8 +120,7 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
         if query.startswith('collection.'):
             user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
             user_anime_characters = sum(c['anime'] == character['anime'] for c in user['characters'])
-            caption = f"<b>⫸OᴡO ʟᴏᴏᴋ ᴀᴛ <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'ꜱ ᴡᴀɪꜰᴜ</b>\n\n<b>• {character['anime']} ({user_anime_characters}/{anime_characters})</b>\n<b>• {character['id']}: </b>{character['name']} (x{user_character_count})\n<b>• ({character['rarity'][0]}𝗥𝗔𝗥𝗜𝗧𝗬: {character['rarity'][2:]})</b>"
-             # Check for tags in character's name
+            caption = f"<b>⫸OᴡO ʟᴏᴏᴋ ᴀᴛ <a href='tg://user?id={str(user['id'])}'>{escape(user.get('first_name', str(user['id'])))}</a>'ꜱ ᴡᴀɪꜰᴜ</b>\n\n"       # Check for tags in character's name
 
             if '👘' in character['name']:
 
