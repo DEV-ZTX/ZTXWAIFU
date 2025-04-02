035b7cd6f29e137113ef668ce2bd027da0c3b535
DEV-ZTX <sucidaldazai777@gmail.com>
2025-04-02 09:54:19 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,19 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7757912959"
-    sudo_users = "7757912959", "7783550524" , "7038202445"
-    GROUP_ID = -1002324830715
-    TOKEN = "8133429097:AAGjkA0pOY3M-J9zS8zZacIq6XGCzyUCyds"
-    mongo_url = "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://graph.org/file/34ce254537a31ca2a788b-f8b59bff546af8dc58.jpg", "https://graph.org/file/0cd48cfd409203a00da74-2a919fe99dd1b238ec.jpg"]
-    SUPPORT_CHAT = "https://t.me/Anime_Group_India_Chat"
-    UPDATE_CHAT = "https://t.me/Shorekeeper_updates"
-    BOT_USERNAME = "HosinoXharembot"
-    CHARA_CHANNEL_ID = "-1002324830715"
-    api_id = 23028479
-    api_hash = "c1e6a93b04c0810a5c282d8d8d44ea6f"
+    OWNER_ID = "8177810307"
+    sudo_users = "7757912959", "7678359785"
+    GROUP_ID = -1002653736596
+    TOKEN = "7964376351:AAFydh26PEkefcCIocN9KSEz0oH61lCLcv4"
+    mongo_url = "mongodb+srv://Lord_ichigo:Roshni@cluster0.ytuss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    PHOTO_URL = ["https://graph.org/file/25846350270d888d09881-2c50ff773c6ae8ffbc.jpg"]
+    SUPPORT_CHAT = "Indian_Anime_Chat_Group"
+    UPDATE_CHAT = "Beast_Tuhin"
+    BOT_USERNAME = "CosplayGusserBot"
+    BOT_NAME = "𝗚𝘂𝗲𝘀𝘀 𝗧𝗵𝗲 𝗖𝗼𝘀𝗽𝗹𝗮𝘆 ꕥ"
+    CHARA_CHANNEL_ID = "-1002293723372"
+    api_id = 23568641
+    api_hash = "a39098e8752a45c2d6d1889941547bbc"
 
     STRICT_GBAN = True
     ALLOW_CHATS = True
@@ -21,10 +22,16 @@ class Config(object):
     DEL_CMDS = True
     INFOPIC = True
 
+
 class Production(Config):
     LOGGER = True
 
 
 class Development(Config):
     LOGGER = True
-    
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
