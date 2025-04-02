ba7c998a5117d2eb76771e64a68357742d4e8549
DEV-ZTX <sucidaldazai777@gmail.com>
2025-04-02 16:52:56 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -117,150 +117,65 @@ async def send_image(update: Update, context: CallbackContext) -> None:
         caption=f"""<b>{character['rarity'][0]}Oᴡᴏ! ᴀ {character['rarity'][2:]} ᴄᴏsᴘʟᴀʏ ʜᴀs ᴀᴘᴘᴇᴀʀᴇᴅ!</b>\n<b>ᴀᴅᴅ ʜᴇʀ ᴛᴏ ʏᴏᴜʀ ʜᴀʀᴇᴍ ʙʏ sᴇɴᴅɪɴɢ</b>\n<b>/grab ɴᴀᴍᴇ</b>""",
         parse_mode='HTML')
 
-
 async def guess(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
-
+    
     if chat_id not in last_characters:
         return
-
-    guess = ' '.join(context.args).lower() if context.args else ''
     
-    if "()" in guess or "&" in guess.lower():
-        await update.message.reply_html("<b>❌️ ʏᴏᴜ ᴄᴀɴ'ᴛ ᴜꜱᴇ ᴛʜɪꜱ ᴛʏᴘᴇꜱ ᴏꜰ ᴡᴏʀᴅꜱ.</b>")
+    if chat_id in first_correct_guesses:
+        await update.message.reply_text('❌️ Already Guessed By Someone.. Try Next Time Bruhh ')
+        return
+    
+    guess = ' '.join(context.args).lower() if context.args else ''
+    if "()" in guess or "&" in guess:
+        await update.message.reply_text("Nahh You Can't use This Types of words in your guess..❌️")
         return
 
     name_parts = last_characters[chat_id]['name'].lower().split()
-
     if sorted(name_parts) == sorted(guess.split()) or any(part == guess for part in name_parts):
-        # Calculate the time taken
-        time_sent = sent_characters[chat_id].get(last_characters[chat_id]['id'], time.time())
-        time_taken = time.time() - time_sent
-        minutes, seconds = divmod(int(time_taken), 60)
-        
         first_correct_guesses[chat_id] = user_id
-        
         user = await user_collection.find_one({'id': user_id})
+        
         if user:
-            update_fields = {}
-            if hasattr(update.effective_user, 'username') and update.effective_user.username != user.get('username'):
-                update_fields['username'] = update.effective_user.username
-            if update.effective_user.first_name != user.get('first_name'):
-                update_fields['first_name'] = update.effective_user.first_name
-            if update_fields:
-                await user_collection.update_one({'id': user_id}, {'$set': update_fields})
-            
-            await user_collection.update_one({'id': user_id}, {'$push': {'characters': last_characters[chat_id]}})
-      
-        elif hasattr(update.effective_user, 'username'):
-            await user_collection.insert_one({
-                'id': user_id,
-                'username': update.effective_user.username,
-                'first_name': update.effective_user.first_name,
-                'characters': [last_characters[chat_id]],
-            })
-
-        keyboard = [[InlineKeyboardButton(f"🌐 ꜱᴇᴇ ᴄᴏʟʟᴇᴄᴛɪᴏɴ", switch_inline_query_current_chat=f"collection.{user_id}")]]
-
-        await update.message.reply_text(f'✅ <b><a href="tg://user?id={user_id}">{escape(update.effective_user.first_name)}</a></b> You got a new character! \n\n🌸𝗡𝗔𝗠𝗘: <b>{last_characters[chat_id]["name"]}</b> \n❇️𝗔𝗡𝗜𝗠𝗘: <b>{last_characters[chat_id]["anime"]}</b> \n{last_characters[chat_id]["rarity"][0]}𝗥𝗔𝗜𝗥𝗧𝗬: <b>{last_characters[chat_id]["rarity"]}</b>\n\n⌛️ 𝗧𝗜𝗠𝗘 𝗧𝗔𝗞𝗘𝗡: {minutes} minutes and {seconds} seconds', parse_mode='HTML', reply_markup=InlineKeyboardMarkup(keyboard))
-
+            await user_collection.update_one({'id': user_id}, {'$set': {'username': update.effective_user.username, 'first_name': update.effective_user.first_name}, '$push': {'characters': last_characters[chat_id]}})
+        else:
+            await user_collection.insert_one({'id': user_id, 'username': update.effective_user.username, 'first_name': update.effective_user.first_name, 'characters': [last_characters[chat_id]]})
+        
+        keyboard = [[InlineKeyboardButton("See Harem", switch_inline_query_current_chat=f"collection.{user_id}")]]
+        await update.message.reply_text(
+            f'<b><a href="tg://user?id={user_id}">{escape(update.effective_user.first_name)}</a></b> You Guessed a New Character ✅️\n\n𝗖𝗢𝗦𝗣𝗟𝗔𝗬: <b>{last_characters[chat_id]["name"]}</b>\n𝗔𝗡𝗜𝗠𝗘: <b>{last_characters[chat_id]["anime"]}</b>\n𝗥𝗔𝗜𝗥𝗧𝗬: <b>{last_characters[chat_id]["rarity"]}</b>\n\nThis Character added in Your harem.. use /harem To see your harem', 
+            parse_mode='HTML', reply_markup=InlineKeyboardMarkup(keyboard))
     else:
-        await update.message.reply_text('❌️<b>ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴀᴍᴇ ɪs ɴᴏᴛ ᴄᴏʀʀᴇᴄᴛ.ᴛʀʏ ɢᴜᴇssɪɴɢ ᴛʜᴇ ɴᴀᴍᴇ ᴀɢᴀɪɴ!</b>', parse_mode='HTML')
-   
-
-"""async def fav(update: Update, context: CallbackContext) -> None:
-    user_id = update.effective_user.id
-
-    if not context.args:
-        await update.message.reply_text('<b>ɢɪᴠᴇ ᴍᴇ ᴀ ᴄᴏsᴘʟᴀʏ ɪᴅ ᴛᴏᴏ 🤖</b>', parse_mode='HTML')
-        return
-
-    character_id = context.args[0]
-
-    user = await user_collection.find_one({'id': user_id})
-    if not user:
-        await update.message.reply_text('<b>ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴏsᴘʟᴀʏ ɪɴ ʏᴏᴜʀ ʜᴀʀᴇᴍ 😢</b>', parse_mode='HTML')
-        return
-
-    character = next((c for c in user['characters'] if c['id'] == character_id), None)
-    if not character:
-        await update.message.reply_text('<b>ʏᴏᴜ ᴅᴏɴᴛ ᴏᴡɴ ᴛʜɪꜱ ᴡᴀɪꜰᴜ🤨</b>', parse_mode='HTML')
-        return
-
-    user['favorites'] = [character_id]
-
-    await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user['favorites']}})
-
-    # Send the character's photo
-    await context.bot.send_photo(
-        chat_id=update.effective_chat.id,
-        photo=character['img_url'],
-        caption=(f'<b>{character["rarity"][0]}ᴡᴀɪꜰᴜ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴀᴅᴅᴇᴅ ᴛᴏ ʏᴏᴜʀ ꜰᴀᴠ\n\n ᴡᴀɪꜰᴜ ɴᴀᴍᴇ: {character["name"]}</b>'
-    ), parse_mode='HTML'
-)
-
-"""
+        await update.message.reply_text('Please Write Correct Character Name... ❌️')
 
 async def fav(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
-
     if not context.args:
-        await update.message.reply_html('<b>ɢɪᴠᴇ ᴍᴇ ᴀ ᴡᴀɪғᴜ ɪᴅ ᴛᴏᴏ 🤖</b>')
+        await update.message.reply_text('Please provide Character id...')
         return
-
+    
     character_id = context.args[0]
     user = await user_collection.find_one({'id': user_id})
-
     if not user:
-        await update.message.reply_html('<b>ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴏsᴘʟᴀʏ ɪɴ ʏᴏᴜʀ ʜᴀʀᴇᴍ 😢</b>')
+        await update.message.reply_text('You have not Guessed any characters yet....')
         return
-
+    
     character = next((c for c in user['characters'] if c['id'] == character_id), None)
     if not character:
-        await update.message.reply_html('<b>ʏᴏᴜ ᴅᴏɴᴛ ᴏᴡɴ ᴛʜɪꜱ ᴡᴀɪꜰᴜ🤨</b>')
+        await update.message.reply_text('This Character is Not In your collection')
         return
-
-    buttons = [
-        [InlineKeyboardButton("🟢 Yes", callback_data=f"yes_{character_id}"), 
-         InlineKeyboardButton("🔴 No", callback_data=f"no_{character_id}")]
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-
-    await update.message.reply_photo(
-        photo=character["img_url"],
-        caption=f"<b>Do you want to make this character your favorite..!</b>\n↬ <code>{character['name']}</code> <code>({character['anime']})</code>",
-        reply_markup=reply_markup,
-        parse_mode='HTML'
-    )
-
-async def handle_yes(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    await query.answer()
-
-    user_id = query.from_user.id
-    character_id = query.data.split('_')[1]
-
+    
     await user_collection.update_one({'id': user_id}, {'$set': {'favorites': [character_id]}})
-    await query.edit_message_caption(caption="<b>ᴡᴀɪғᴜ ʜᴀs ʙᴇᴇɴ sᴜᴄᴄᴇssғᴜʟʟʏ sᴇᴛ ᴀs ᴀ ғᴀᴠᴏʀɪᴛᴇ!</b>", parse_mode="HTML")
-
-async def handle_no(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    await query.answer("Okay, no worries!")
-    await query.edit_message_caption(caption="canceled.")
+    await update.message.reply_text(f'Character {character["name"]} has been added to your favorite...')
 
 def main() -> None:
-    """Run bot."""
-    
-    application.add_handler(CommandHandler(["guess"], guess, block=False))
-    application.add_handler(CommandHandler('fav', fav))
-    application.add_handler(CallbackQueryHandler(handle_yes, pattern="yes_*"))
-    application.add_handler(CallbackQueryHandler(handle_no, pattern="no_*"))
-    #application.add_handler(CommandHandler("fav", fav, block=False))
+    application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
+    application.add_handler(CommandHandler("fav", fav, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
-
     application.run_polling(drop_pending_updates=True)
-    
+
 if __name__ == "__main__":
     shivuu.start()
     LOGGER.info("Bot started")
